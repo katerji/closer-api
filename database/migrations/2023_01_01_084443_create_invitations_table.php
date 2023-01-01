059f2ba9bbd4deb208934072fdb9948a27df9f61
katerji <katerji.abedalaziz@gmail.com>
2023-01-01 13:20:17 +0400

    set unique key user_id contact_user_id to avoid duplicate invitations/contacts

--- database/migrations/2023_01_01_084443_create_invitations_table.php
+++ database/migrations/2023_01_01_084443_create_invitations_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('contact_user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
+            $table->unique(['user_id', 'contact_user_id']);
         });
     }
 
