b3545d3e4efd3e0581a89080d250abe17431f225
katerji <katerji.abedalaziz@gmail.com>
2023-01-01 13:15:11 +0400

    adapt auth controller to register and log in based on phone instead of email

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
      */
     public function login(Request $request){
     	$validator = Validator::make($request->all(), [
-            'email' => 'required|email',
+            'phone' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/',
             'password' => 'required|string|min:6',
         ]);
         if ($validator->fails()) {
@@ -41,7 +41,7 @@ class AuthController extends Controller
     public function register(Request $request) {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|between:2,100',
-            'email' => 'required|string|email|max:100|unique:users',
+            'phone' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|unique:users',
             'password' => 'required|string|confirmed|min:6',
         ]);
         if($validator->fails()){
